be3a857897afd099fd7859b8eaea1df1cc9d0994
JamesHahn3919 <james.hahn3919@gmail.com>
2019-08-16 21:07:38 -0700

    render error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import RoomList from "./components/RoomList";
+import * as firebase from "firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCxp4t4ljHRHg7RqTSOWyVtghJgqdJ7Sts",
+  authDomain: "bloc-chat-d3e97.firebaseapp.com",
+  databaseURL: "https://bloc-chat-d3e97.firebaseio.com",
+  projectId: "bloc-chat-d3e97",
+  storageBucket: "",
+  messagingSenderId: "598283045534",
+  appId: "1:598283045534:web:2e462abf3492ebe7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="RoomList">
+        <RoomList firebase={firebase.app()} />
+      </div>
     </div>
   );
 }
